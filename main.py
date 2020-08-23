faf47dc25a6998205cb1f953643580faf14bf66e
O <48214249+OlegKorn@users.noreply.github.com>
2020-08-23 18:11:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,100 +1,64 @@
-import requests, sys
 from bs4 import BeautifulSoup as bs
+import requests
+
+URLS = [
+    'https://xuk.name/erotic/ekaterina-5/14648',
+    'https://xuk.name/erotic/ekaterina-4/13913',
+    'https://xuk.name/erotic/ekaterina-3/13614',
+    'https://xuk.name/erotic/other/24636',
+    'https://xuk.name/erotic/other/23314',
+    'https://xuk.name/erotic/other/22894',
+    'https://xuk.name/erotic/ekaterina-20/19027',
+    'https://xuk.name/erotic/ekaterina-18/18444',
+    'https://xuk.name/erotic/ekaterina-15/18127',
+    'https://xuk.name/erotic/ekaterina-9/16345',
+    'https://xuk.name/erotic/other/29263',
+    'https://xuk.name/erotic/ekaterina-14/17271',
+    'https://xuk.name/erotic/ekaterina-10/16751',
+    'https://xuk.name/erotic/ekaterina-7/15611',
+    'https://xuk.name/erotic/other/31393'
+]
+
+links = 'G:/Desktop/links.txt'
+home = 'G:/Desktop/ЭКОЛОГИЯ/'
 
 
-URL = 'https://xuk.ooo/erotic/blonde/page'
+def get_links():
+    for url in URLS:
+        try:
+            session = requests.Session()
+            request = session.get(url)
+            soup = bs(request.content, 'html.parser')
 
-headers = {'access-control-allow-origin' : '*',
-           'Request Method' : 'GET',
-           'Status Code' : '200',
-           'Remote Address' : '64.233.163.101:443',
-           'Referrer Policy' : 'no-referrer-when-downgrade',
-           'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
-}
+            for i in soup.find('div', attrs={'class':'photo-items grid tiles'}).find_all('div', class_='photo-item'):
+                link = i.a.get('href')
+                    
+                f.write(link)
+                f.write('\n')
 
-home = '/home/o/python/ero/blond10_22/'
-
-bl = '/home/o/python/ero/blond10_22/blond10_22.txt'
-
-stop = 22
-
-#f = open(bl, 'w')
-
-def get_entries_urls(n:int):
-    
-    try:
-        if n == stop:
-            f.close()
-            sys.exit()
-
-        print(n)
-
-        session = requests.Session()
-        request = session.get(URL + str(n), headers=headers)
-        soup = bs(request.content, 'html.parser')
-
-        for i in soup.find('div', attrs={'class':'items justified'}).find_all('div', class_='photo-item'):
-            link = i.a.get('href')
-               
-            f.write(link)
-            f.write('\n')
-        get_entries_urls(n+1)
-
-    except Exception as e:
-        print(e)
-        pass
+        except Exception as e:
+            print(e)
+            pass
 
     f.close()
 
 
-
-def save():
-    
-    f = open(bl, 'r').readlines()
-    
+def save_img():
+    f = open(links, 'r').readlines()
     try:
-        
         for entry_url in f:
             session = requests.Session()
-            request = session.get(entry_url, headers=headers)
+            request = session.get(entry_url.strip())
             soup = bs(request.content, 'html.parser')
-            for img in soup.find('div', id='items-container').find_all('div', class_='photo-item'): 
-                try:
-                    img_prelink = img.a.get('href')
-                    session = requests.Session()
-                    request = session.get(img_prelink, headers=headers)
-                    img_soup = bs(request.content, 'html.parser')
-                    img_link = img_soup.find('div', class_='photo-info')
-                    end_link = img_link.a.get('href')
-                    print(end_link)
-                    r = requests.get(end_link, stream=True)
-                    image = r.raw.read()
-                    open(home + end_link[60:], "wb").write(image)
-                except Exception:
-                    pass
+            img = soup.find('img', class_='photomodel-image').get('src')
+            
+            print(img)
+            r = requests.get(img, stream=True)
+            image = r.raw.read()
+            open(home + img[70:], "wb").write(image)
 
     except Exception:
         pass
 
-#get_entries_urls(10)
-save()
 
-
-
-
-
-
-
-
-
-
-# download the url contents in binary format
-#r = requests.get(url, stream=True)
-#image = r.raw.read()
-
-#print(r.status_code)
-
-# open method to open a file on your system and write the contents
-#if r.status_code == 200:
-
-#open("/home/o/Загрузки/dd.jpg", "wb").write(image)
+save_img()
