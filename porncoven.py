a35fade58c793808ff0cec7875b136382ed661f9
O <48214249+OlegKorn@users.noreply.github.com>
2022-04-08 08:48:05 +0800

    Update porncoven.py

--- porncoven.py
+++ porncoven.py
@@ -1,3 +1,19 @@
+let a = document.querySelectorAll("[id^='post_message']")
+
+arr_ = []
+
+for (i=0; i<a.length; i++) {
+    let links = a[i].querySelectorAll("a")
+    
+    for (j=0; j<links.length; j++) {
+        link = links[j]["href"]
+            arr_.push(link)        
+    }
+}
+arr_
+
+
+
 from bs4 import BeautifulSoup as bs
 import requests
 import re
