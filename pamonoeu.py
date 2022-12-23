d315d0fc82978a469735d19dfe23a33333bed71f
O <48214249+OlegKorn@users.noreply.github.com>
2022-12-23 11:35:44 +0800

    Update pamonoeu.py
    try:
        f = get_filter()
        print(f)
        
        if "\?design" in url:
            search_wo_query = re.search(r'eu/(.*?)\?design', url).group(1)
            if search_wo_query:
                path = f"/home/oleg/Public/py/{search_wo_query}_{f}/"
                print(path)
                log_name = f"{search_wo_query}_{f}"
                create_dir(path)        
                
        if "&q=" in url and not f:
            search = re.search("&q=(.*)", url).group(1)
            if search:
                path = f"/home/oleg/Public/py/{search}_{f}/"
                print(path)
                log_name = f"{search}_{f}"
                create_dir(path)
     
        if "design_period_new" or "style" in url:
            path = f"/home/oleg/Public/py/{f}"
            log_name = f"{path}/log.log"
            print(path, log_name)
            create_dir(path)

--- pamonoeu.py
+++ pamonoeu.py
@@ -11,48 +11,63 @@ headers = {
 }
 
 page = 1
-url = f"https://www.pamono.eu/work-on-paper-figurative?design_period_new=941&p={str(page)}"
+url = "https://www.pamono.eu/catalogsearch/result/index/?cat=1465&" + \
+      f"design_period_new=943%2C944%2C956&p={str(page)}&q=painting&style=2921%2C2931%2C4250"
 
 
 def get_filter(): 
     s = requests.Session()
     r = s.get(url, headers=headers)
     soup = bs(r.content, 'html.parser')
-    filtr = soup.find("div", class_="filter-state").find("span", class_="value").text.strip()
 
-    return filtr
+    filters_merged = ""
+
+    filters = soup.find("ol", class_="items").find_all("span", class_="value")
+    for f in filters:
+        f = f.text.strip()
+        filters_merged = filters_merged + f + ","
+
+    filters_merged = filters_merged[:-1]
+    print("huj = ", filters_merged)
+    return filters_merged 
+
+
+def create_dir(path):
+    if os.path.exists(path):
+        print(path, " exists")
+    if not os.path.exists(path):
+        os.makedirs(path, mode=0o777)
+
 
 try:
     f = get_filter()
-
-    search_wo_query = re.search(r'eu/(.*?)\?design', url).group(1)
-    if search_wo_query:
-        path = f"/home/oleg/Public/py/{search_wo_query}_{f}/"
-        print(path)
-        log_name = f"{search_wo_query}_{f}"
-                        
-        if os.path.exists(path):
-            print(path, " exists")
-
-        if not os.path.exists(path):
-            os.makedirs(path, mode=0o777)
-
-    if not search_wo_query:
+    print(f)
+    
+    if "\?design" in url:
+        search_wo_query = re.search(r'eu/(.*?)\?design', url).group(1)
+        if search_wo_query:
+            path = f"/home/oleg/Public/py/{search_wo_query}_{f}/"
+            print(path)
+            log_name = f"{search_wo_query}_{f}"
+            create_dir(path)        
+            
+    if "&q=" in url and not f:
         search = re.search("&q=(.*)", url).group(1)
         if search:
             path = f"/home/oleg/Public/py/{search}_{f}/"
             print(path)
             log_name = f"{search}_{f}"
-                            
-            if os.path.exists(path):
-                print(path, " exists")
-                            
-            if not os.path.exists(path):
-                os.makedirs(path, mode=0o777)
-                                        
+            create_dir(path)
+ 
+    if "design_period_new" or "style" in url:
+        path = f"/home/oleg/Public/py/{f}"
+        log_name = f"{path}/log.log"
+        print(path, log_name)
+        create_dir(path)
+
+                  
 except Exception as e:
     print(e)
-
 
 
 class Pamono:
@@ -101,7 +116,7 @@ class Pamono:
     def write_links_of_fotos_of_an_item(self, url):
         FORMAT = '%(message)s'
         logging.basicConfig(
-            filename=path + log_name + ".log",
+            filename=log_name, #path + log_name + ".log",
             level=logging.INFO,
             format=FORMAT
         )
@@ -122,8 +137,8 @@ class Pamono:
 
     def download_file(self):
         print("DOWNLOADING BEGAN...")
+        print(log)
 
-        log = path + log_name + ".log"
         f = open(log, "r")
 
         for link in f.readlines():
@@ -137,10 +152,13 @@ class Pamono:
 
 
 p = Pamono()
-
 last_page = p.get_last_page(url)
+
+
 for page in range(1, int(last_page)+1):
-    url = f"https://www.pamono.eu/work-on-paper-figurative?design_period_new=941&p={str(page)}"
+    url = "https://www.pamono.eu/catalogsearch/result/index/?cat=1465&" + \
+          f"design_period_new=943%2C944%2C956&p={str(page)}&q=painting&style=2921%2C2931%2C4250"
+    print(url)
 
     items = p.get_items_of_page(url)
 
@@ -148,6 +166,7 @@ for page in range(1, int(last_page)+1):
         print("\n-----------------------")
         print(f"{item}, page={page} of {last_page}, item number={items.index(item)} of {len(items)}")
         p.write_links_of_fotos_of_an_item(item)
+
 
 p.download_file()
 
