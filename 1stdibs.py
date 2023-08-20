d1551f5d68cd5ac95c030e45482f82201bdae54f
O <48214249+OlegKorn@users.noreply.github.com>
2023-08-20 14:39:30 +0800

    Update 1stdibs.py
    def delete_forbidden_chars(string):
        string = re.sub('['+FORBIDDEN_CHARS+']', '', string).replace('"', "")
        string = unidecode(string) 
    
        return string

--- 1stdibs.py
+++ 1stdibs.py
@@ -19,6 +19,8 @@ BASE_PATH = "G:/Desktop/py/1stdibs/"
 
 url = "https://www.1stdibs.com/furniture/decorative-objects/?per=1910s,1920s,early-1900s"
 
+# deleting forbidden chars
+FORBIDDEN_CHARS = re.escape(punctuation)
 
 def get_filters():
     fil = re.search("/?per=(.*)$", url).group(1).replace(",", "_") + category
@@ -32,6 +34,13 @@ def create_dir(filters=get_filters()):
        
     if not os.path.exists(p):
         os.makedirs(p, mode=0o777)
+
+def delete_forbidden_chars(string):
+    string = re.sub('['+FORBIDDEN_CHARS+']', '', string).replace('"', "")
+    string = unidecode(string) 
+
+    return string
+
 
 
 class Pizda:
@@ -53,7 +62,7 @@ class Pizda:
             return False
 
         return self.last_page
-
+ 
 
     def write_all_anchors_of_found_items(self):
         page = 1
@@ -95,7 +104,6 @@ class Pizda:
 
 
     def write_author_title_and_url_of_one_photo(self):
-
         FORMAT = '%(message)s'
         logging.basicConfig(
             filename= BASE_PATH + get_filters() + "/" + get_filters() + "_authors_titles_and_urls_of_photos.log",
@@ -103,42 +111,45 @@ class Pizda:
             format=FORMAT
         )
 
-        all_items_anchors = open(BASE_PATH + get_filters() + "/" + get_filters() + "_all_items_anchors.log", "r")
+        with open(BASE_PATH + get_filters() + "/" + get_filters() + "_all_items_anchors.log", "r+") as _all_items_anchors:
+            
+            lines = _all_items_anchors.readlines()
        
-        for line in all_items_anchors.readlines():
-            soup = self.get_soup(line.strip())                
-            src = soup.find("button", attrs={"data-tn": "pdp-image-carousel-image-1"}).find("img")["src"].replace("?width=768", "")
-     
-            # deleting forbidden chars
-            forbidden_chars = re.escape(punctuation)
-           
-            try:
-                author_and_title = soup.find("h1", attrs={"data-tn": "pdp-resp-title"}).text
-
-                # deleting forbidden chars
-                author_and_title = re.sub('['+forbidden_chars+']', '', author_and_title)
-                author_and_title = unidecode(author_and_title)          
-            except:
-                author_and_title = "Unknown Author"
-             
-            try:
-                year = soup.find("p", attrs={"data-tn": "pdp-item-creation-date"}).text
-            except:
-                year = "Unknown Year"
+            for line in lines:
+                soup = self.get_soup(line.strip())                
+                src = soup.find("button", attrs={"data-tn": "pdp-image-carousel-image-1"}).find("img")["src"].replace("?width=768", "")
                
-            data = author_and_title + "++" + year + "++" + src
-           
-            print("===========")
-            print("URL: ", "..." + line[20:].strip())
-            print(line.strip())
-            print(author_and_title)
-            print()
+                try:
+                    author_and_title = delete_forbidden_chars(soup.find("h1", attrs={"data-tn": "pdp-resp-title"}).text)
+                    # author_and_title = delete_forbidden_chars(author_and_title)
+                except:
+                    author_and_title = "Unknown Author"
+                 
+                try:
+                    year = soup.find("p", attrs={"data-tn": "pdp-item-creation-date"}).text
+                except:
+                    year = "Unknown Year"
+                   
+                data = author_and_title + "++" + year + "++" + src
+               
+                print("===========")
+                print("URL: ", "..." + line[20:].strip())
+                print(line.strip())
+                print(author_and_title)
+                print()
 
-            try:
-                logging.info(data)
-            except Exception as e:
-                print(e)
-                continue
+                try:
+                    logging.info(data)
+
+                    # deleting 1st line 
+                    _all_items_anchors.seek(0)
+                    _all_items_anchors.truncate()
+
+                    _all_items_anchors.writelines(lines[1:])
+
+                except Exception as e:
+                    print(e)
+                    break
 
 
     def download_file(self):
@@ -150,9 +161,10 @@ class Pizda:
             for link in f.readlines():
                 url = link.split('++')[2].strip()
                
-                author_and_title = link.split('++')[0].strip()
+                author_and_title = delete_forbidden_chars(link.split('++')[0].strip())
+                # author_and_title = delete_forbidden_chars(author_and_title)
                
-                print(author_and_title, url, sep=" - ")
+                print(author_and_title, url, sep=" ---- ")
                 print("++++++++++++")
 
                 p = BASE_PATH + get_filters()
@@ -170,5 +182,5 @@ create_dir()
 
 p = Pizda()
 # p.write_all_anchors_of_found_items()
-p.write_author_title_and_url_of_one_photo()
-# p.download_file()
+# p.write_author_title_and_url_of_one_photo()
+p.download_file()
